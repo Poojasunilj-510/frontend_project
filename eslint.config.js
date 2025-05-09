563a54412312299d2bd0ad0966f04dad2ca0c1df
Pooja Jadhav <pooja.140.cse4b@gmail.com>
2025-05-09 14:41:26 +0900

    added prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,13 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier'; // ✅ Import Prettier config
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -25,4 +25,5 @@ export default tseslint.config(
       ],
     },
   },
-)
+  prettier // ✅ Add Prettier as the last config to disable conflicting rules
+);
