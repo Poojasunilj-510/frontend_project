43645599ae6fb43f49487f8ae0fac378317cbfed
Pooja Jadhav <pooja.140.cse4b@gmail.com>
2025-05-14 20:45:47 +0900

    updated necessary changes for app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ChakraProvider, ThemeConfig, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { MoviesProvider } from './context/movies-context';
@@ -8,43 +8,9 @@ import HomePage from './pages/home-page';
 import FavoritesPage from './pages/favorites-page';
 import MovieDetailPage from './pages/movie-detail-page';
 
-const config: ThemeConfig = {
-  initialColorMode: 'light',
-  useSystemColorMode: false,
-};
-
-const theme = extendTheme({
-  config,
-  fonts: {
-    body: 'Inter, system-ui, sans-serif',
-    heading: 'Inter, system-ui, sans-serif',
-  },
-  colors: {
-    brand: {
-      50: '#e6f5ff',
-      100: '#b3d9ff',
-      200: '#80bdff',
-      300: '#4da0ff',
-      400: '#1a85ff',
-      500: '#006ae6',
-      600: '#0052b3',
-      700: '#003980',
-      800: '#00214d',
-      900: '#00081a',
-    },
-  },
-  styles: {
-    global: {
-      body: {
-        bg: 'gray.50',
-      },
-    },
-  },
-});
-
 function App() {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <MoviesProvider>
         <Router>
           <Layout>
